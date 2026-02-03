146001a6672cab675b02757a35e83315c0bdb450
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2026-02-03 19:20:01 +0600

    chore:mongodb atlas connected

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/sprintplanner", {
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true,
-        });
+        const dbName = process.env.MONGODB_DBNAME || 'sprint_planner';
+        const uri = `mongodb+srv://${encodeURIComponent(process.env.MONGODB_USERNAME)}:${encodeURIComponent(process.env.MONGODB_PASSWORD)}@cluster0.izfa076.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+        await mongoose.connect(uri, { dbName });
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection failed:', error.message);
